b27ae8eade78453ff3f23a39d06626549e1fb5ca
小毅 <1030129479@qq.com>
2018-09-17 19:02:12 +0800

    提交文档

---
+++ CustomTools/CustomTools/Classes/Main/Base/Model/BaseModel.h
@@ -0,0 +1,17 @@
+//
+//  BaseModel.h
+//  CustomTools
+//
+//  Created by 小毅 on 2018/9/17.
+//  Copyright © 2018年 小毅. All rights reserved.
+//
+//  作者Github址: https://github.com/CoderLanni
+//	作者CSDN博客地址: https://blog.csdn.net/qq_19678579
+// 
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseModel : NSObject
+
+@end
